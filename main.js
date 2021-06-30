6dfbc25c753a0bd63e001cb2ff9d94097a33c9e3
Iryna <irinabolbat98@gmail.com>
2021-06-30 16:58:15 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// const result = confirm('Are you here?');
+// console.log(result);
+
+
+// const answer = prompt('Are you 18?');
+// console.log(answer);
+
+const answers =[];
+
+answers[0] = prompt('What is your name?');
+answers[1] = prompt('What is your sirname?');
+answers[2] = prompt('How old are you?');
+
+document.write(answers);
